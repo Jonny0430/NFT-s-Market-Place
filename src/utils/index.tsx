3c62a91453992f56d544208582591e1617ac93f8
Jonny0430 <ahadjon697gmail.com>
2025-07-18 13:29:51 +0900

    feat: Market Place Where NFT's Sold & Hold

---
+++ src/utils/index.tsx
@@ -0,0 +1,72 @@
+export const NavLinks = [
+  {
+    id: "marketplace",
+    name: "marketplace",
+  },
+  {
+    id: "about",
+    name: "About",
+  },
+  {
+    id: "resources",
+    name: "resources",
+  },
+  {
+    id: "faq",
+    name: "FAQ",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "marketplace",
+    links: [
+      {
+        name: "explore",
+      },
+      {
+        name: "NFTs",
+      },
+      {
+        name: "collectibles",
+      },
+      {
+        name: "virtual reality",
+      },
+    ],
+  },
+  {
+    title: "resources",
+    links: [
+      {
+        name: "Help Center",
+      },
+      {
+        name: "Partners",
+      },
+      {
+        name: "Blog",
+      },
+      {
+        name: "Newsletters",
+      },
+    ],
+  },
+  {
+    title: "company",
+    links: [
+      {
+        name: "about us",
+      },
+      {
+        name: "careers",
+      },
+      {
+        name: "support",
+      },
+      {
+        name: "newsletter",
+      },
+    ],
+  },
+];
